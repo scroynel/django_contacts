0436815235bf8e150c23a3b35a54008bbf5f5860
Vladyslav <scroynel@gmail.com>
2026-03-01 15:32:09 +0100

    Creating template for list contact,  view

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import contacts
+from .views import contacts, ContactListView
 from .autocomplete_view import CityAutocomplete
 
 
 urlpatterns = [
-    path('', contacts, name='contacts'),
-    path(
-        'city-autocomplete/',
-        CityAutocomplete.as_view(),
-        name='city-autocomplete'
-    ),
+    path('', ContactListView.as_view(), name='contacts'),
+    path('city-autocomplete/', CityAutocomplete.as_view(), name='city-autocomplete'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Contact
 
 
 def contacts(request):
     return render(request, template_name='contacts/contacts.html') 
+
+
+class ContactListView(ListView):
+    queryset = Contact.objects.all()
+    template_name = 'contacts/contacts.html'
+    context_object_name = 'contacts'
